37ee28708f29ab591c9e5cad1a93012280af20ce
bwangel23 <bwangel.me@gmail.com>
2016-10-27 15:39:48 +0800

    fix(hsize): fix the import bug

--- hsize/__init__.py
+++ hsize/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from hsize import human2bytes
-from hsize import bytes2human
+from .hsize import human2bytes
+from .hsize import bytes2human
 
 __all__ = [
     'human2bytes',
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'hsize',
-    version = '0.0.1',
+    version = '0.0.2',
     keywords = ('human', 'size'),
     description = 'Convert the size between human size and bytes on python3',
     license = 'MIT License',
